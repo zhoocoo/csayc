07594ccb443602d3c4c7f4bcdc4d1c8af2968617
zhoocoo <473015937@qq.com>
2022-11-02 10:02:19 +0800

    test(content): nuxt/content引入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // modules: ["@nuxt/content"],
+  modules: ["@nuxt/content"],
   content: {
     // https://content.nuxtjs.org/api/configuration
   },
   nitro: {
     preset: "node-server",
   },
-  // server: {
-  //   port: 3333,
-  //   host: "0.0.0.0",
-  // },
 });
