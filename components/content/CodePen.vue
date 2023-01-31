a168b276eebee1d3166dd8e70f96db54f71e96d7
zhoocoo <473015937@qq.com>
2023-01-31 11:33:18 +0800

    引入codePen的展示形式

---
+++ components/content/CodePen.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="my-4">
+    <button
+      v-if="control"
+      class="btn-warning btn-sm btn my-2"
+      @click="toggle()"
+    >
+      {{ value ? '关闭' : '试一试' }}
+    </button>
+    <iframe
+      v-if="value"
+      :height="height"
+      style="width: 100%"
+      scrolling="no"
+      :title="title"
+      :src="refinedSrc"
+      frameborder="no"
+      loading="lazy"
+      allowtransparency="true"
+      allowfullscreen="true"
+    >
+      See the Pen
+      <a :href="viewUrl"> {{ title }}</a> by {{ nickName }} (<a :href="baseUrl"
+        >@{{ userName }}</a
+      >) on <a :href="penUrl">CodePen</a>.
+    </iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { resolveURL, withQuery } from 'ufo'
+import { useToggle } from '@vueuse/core'
+import { computed } from '#imports'
+
+const props = defineProps({
+  control: {
+    type: Boolean,
+    default: false
+  },
+  height: {
+    type: [String, Number],
+    default: 400
+  },
+  title: {
+    type: String,
+    default: 'Demo'
+  },
+  userName: {
+    type: String,
+    default: 'zhoocoo'
+  },
+  nickName: {
+    type: String,
+    default: 'LogonCong'
+  },
+  id: {
+    type: String,
+    default: ''
+  },
+  defaultTab: {
+    type: Array,
+    default() {
+      return ['result']
+    }
+  },
+  themeId: {
+    type: String,
+    default: 'dark'
+  }
+})
+
+const [value, toggle] = useToggle(!props.control)
+const penUrl = 'https://codepen.io'
+
+const baseUrl = computed(() => resolveURL(penUrl, props.userName))
+
+const iframeUrl = computed(() => {
+  return resolveURL(baseUrl.value, 'embed', props.id)
+})
+
+const viewUrl = computed(() => {
+  return resolveURL(baseUrl.value, 'pen', props.id)
+})
+
+const refinedSrc = computed(() => {
+  const defaultTab = props.defaultTab.join(',')
+  return withQuery(iframeUrl.value, {
+    'theme-id': props.themeId,
+    'default-tab': defaultTab
+  })
+})
+</script>
